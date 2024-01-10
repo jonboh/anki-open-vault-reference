c7cb178c0d8659a8ee596f0b25f5f41bb9391da5
jonboh <jon.bosque.hernando@gmail.com>
2024-01-10 21:05:11 +0100

    set reference format

--- __init__.py
+++ __init__.py
@@ -3,18 +3,25 @@ from aqt.utils import showInfo
 from aqt.qt import *
 from anki.hooks import addHook
 import subprocess
+
+
 def open_wezterm_with_nvim():
     try:
         reviewer = mw.reviewer
         if reviewer.card:
-            note = reviewer.card.note()
-            if 'Vault-Reference' in note:
-                field_value = note['Vault-Reference']
-                if field_value:
-                    command = ["wezterm", "start", "--", "nvim", field_value]
+            anki_note = reviewer.card.note()
+            if 'Vault-Reference' in anki_note:
+                try:
+                    print(anki_note['Vault-Reference'])
+                    note, header  = anki_note['Vault-Reference'].split('<br>')
+                except:
+                    note = None
+                    header = None
+                if note and header:
+                    command = ["wezterm", "start", "--", "nvim", "-c", f"call OpenNeorgHeader('~/doc/vault/{note}', '{header}')"]
                     subprocess.Popen(command)
                 else:
-                    showInfo("No vault reference in current card")
+                    showInfo("Vault-Reference field is not valid")
             else:
                 showInfo("Current note does not have a Vault-Reference field")
         else:
@@ -22,10 +29,12 @@ def open_wezterm_with_nvim():
     except Exception as e:
         showInfo(f"Failed to open WezTerm with nvim: {e}")
 
+
 def setup_shortcut():
     action = QAction("Open WezTerm with nvim", mw)
     action.setShortcut(QKeySequence("Ctrl+A"))
     action.triggered.connect(open_wezterm_with_nvim)
     mw.addAction(action)
+
 
 addHook("profileLoaded", setup_shortcut)
